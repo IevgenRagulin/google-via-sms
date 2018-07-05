aa82914c967c087074496a1d2f05f39a8ef5986e
Ievgen Ragulin <jvyrus@gmail.com>
2018-07-05 13:44:41 -0400

    add readme.md

---
+++ READMD.md
@@ -0,0 +1,1 @@
+This is a project that allows you to Google using SMS. You send a query to a phone number, and in the response you get your search results.
--- app.py
+++ app.py
@@ -25,12 +25,6 @@ def sms_reply():
     google_response = requests.get('https://www.googleapis.com/customsearch/v1?key='+os.environ['GOOGLE_API_KEY']+'&cx='+os.environ['CUSTOM_SEARCH_CONSOLE']+'&q='+body).content
     google_response = json.loads(google_response)
     print(google_response)
-    items = google_response['items']
-    print(items)
-    item_1 = items[0]
-    print(item_1)
-    snippet = item_1['snippet']
-    print(snippet)
     resp.message(google_response['items'][0]['snippet'])
     return str(resp)
 
